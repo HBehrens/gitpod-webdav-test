4520be27f56ad7a0cd22a0f4f45bc2f1c682be36
Heiko Behrens <mail@HeikoBehrens.net>
2021-03-04 23:27:55 +0000

    working screenshots

---
+++ test/images/README.md
@@ -0,0 +1,1 @@
+this folder contains generated screenshots after running `npm test`
--- test/screenshots.ts
+++ test/screenshots.ts
@@ -1,8 +1,32 @@
 import * as puppeteer from 'puppeteer';
 
 (async () => {
-    const browser = await puppeteer.launch();
-	browser.close();
+    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+
+    const page = await browser.newPage();
+    const url = 'http://localhost:8000';
+    const widths = [
+        2560, 	// large 27" cinema displays
+        // 1920, 	// large desktop
+        // 1600, 	// desktop
+        1280, 	// laptop
+        // 1024, 	// tablet-landscape
+        // 768, 	// tablet-portrait
+        // 480, 	// smartphone-landscape
+        320 	// smartphone-portrait
+    ];
+
+    await page.goto(url, { waitUntil: 'networkidle2' });
+
+    for (const width of widths) {
+		await page.setViewport({
+			width: width,
+			height: 500 // this is minimum height
+		});        
+        await page.screenshot({ path: `${__dirname}/images/screenshot-${width}.png`, fullPage: true });
+    }
+
+    await browser.close();
 })().catch((e: Error) => {
     console.error(e);
     process.exit(1);
