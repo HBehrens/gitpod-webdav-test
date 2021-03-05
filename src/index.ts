33b8ef0ef4b7e09f07a18a0a5c3b098daaf8809d
Heiko Behrens <mail@HeikoBehrens.net>
2021-03-05 01:29:41 +0000

    abandoned idea

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 window.onload = () => {
     const headline = document.getElementsByTagName('h1')[0];
-    const who: string = 'World';
+    const who: string = 'World...';
     headline.innerText += who;
 }
--- test/screenshots.ts
+++ test/screenshots.ts
@@ -1,5 +1,7 @@
 import * as puppeteer from 'puppeteer';
 import * as mergeImages from 'merge-img';
+import * as chokidar from 'chokidar';
+import * as pressAnyKey from 'press-any-key';
 
 // console.log(mergeImg);
 // process.exit(0);
@@ -23,25 +25,49 @@ async function takeScreenshots(browser: puppeteer.Browser, url: string) {
     const mergeImgElements = [];
 
     for (const width of widths) {
-		await page.setViewport({
-			width: width,
-			height: 500,
-        });        
+        await page.setViewport({
+            width: width,
+            height: 500,
+        });
         const filename = `${__dirname}/images/screenshot-${width}.png`;
-        mergeImgElements.push({src: filename, offsetX: 1});
+        mergeImgElements.push({ src: filename, offsetX: 1 });
         await page.screenshot({ path: filename, fullPage: true });
     }
 
     const img = await mergeImages(mergeImgElements);
     img.write(`${__dirname}/images/all.png`);
+    console.log("\nscreenshots updated")
 }
 
-(async () => {
-    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
-    await takeScreenshots(browser, "http://localhost:8000");
+const keypress = async () => {
+    process.stdin.setRawMode(true);
+    return new Promise(resolve => process.stdin.once('data', () => {
+        process.stdin.setRawMode(false);
+        resolve(true);
+    }))
+}
 
+function handleAsync<T>(p: Promise<T>) {
+    p.catch((e: Error) => {
+        console.error(e);
+        process.exit(1);
+    });
+}
+
+handleAsync((async () => {
+    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+    const url = "http://localhost:8000";
+    await takeScreenshots(browser, url);
+
+    const watcher = chokidar.watch(`${__dirname}/../src`, {
+        ignoreInitial: true,
+        //   ignored: /(^|[\/\\])\../, // ignore dotfiles
+    });
+
+    watcher.on('all', () => handleAsync(takeScreenshots(browser, url)));
+
+    await pressAnyKey();
+
+    await watcher.close();
     await browser.close();
-})().catch((e: Error) => {
-    console.error(e);
-    process.exit(1);
-});
+})());
